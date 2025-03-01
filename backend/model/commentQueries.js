e0be2bab873fb7f0f83b8243e98ceee08c887c3d
mastachii <mastachii273@gmail.com>
2025-03-01 18:15:18 +0800

    FEAT: Queries for deleting and liking / disliking comments

--- backend/model/commentQueries.js
+++ backend/model/commentQueries.js
@@ -20,17 +20,54 @@ class Comment {
             },
         });
     }
+
+    async deleteComment({ id }) {
+        await prisma.comment.delete({
+            where: { id },
+        });
+    }
+
+    async likeComment({ id, userId }) {
+        await prisma.comment.update({
+            where: { id },
+            data: {
+                likedBy: {
+                    connect: {
+                        id: userId,
+                    },
+                },
+            },
+        });
+    }
+
+    async dislikeComment({ id, userId }) {
+        await prisma.comment.update({
+            where: { id },
+            data: {
+                likedBy: {
+                    disconnect: {
+                        id: userId,
+                    },
+                },
+            },
+        });
+    }
 }
 
 (async () => {
     const db = new Comment();
 
-    await db.createComment({ body: "amazing post!", userId: 2, postId: "871b539e-7745-4c40-97d2-af641be4efaf" });
+    // await db.createComment({ body: "amazing post!", userId: 2, postId: "871b539e-7745-4c40-97d2-af641be4efaf" });
+    await db.dislikeComment({ id: "65e09ebb-1622-4343-9bc8-223d9df7db01", userId: 1 });
 
     const query = await prisma.post.findMany({
         include: {
             likedBy: true,
-            comments: true,
+            comments: {
+                include: {
+                    likedBy: true,
+                },
+            },
         },
     });
 
