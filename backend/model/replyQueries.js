11cc3e701e8f3f1e66dcf0919fcf9fec4d51e647
mastachii <mastachii273@gmail.com>
2025-03-01 19:15:37 +0800

    FEAT: Queries for liking / disliking replies

--- backend/model/replyQueries.js
+++ backend/model/replyQueries.js
@@ -20,20 +20,52 @@ class Reply {
             },
         });
     }
+
+    async likeReply({ id, userId }) {
+        await prisma.reply.update({
+            where: { id },
+            data: {
+                likedBy: {
+                    connect: {
+                        id: userId,
+                    },
+                },
+            },
+        });
+    }
+
+    async dislikeReply({ id, userId }) {
+        await prisma.reply.update({
+            where: { id },
+            data: {
+                likedBy: {
+                    disconnect: {
+                        id: userId,
+                    },
+                },
+            },
+        });
+    }
 }
 
 (async () => {
     const db = new Reply();
 
-    await db.createReply({
-        body: "The comment was persuasive and intellectually stimulating",
-        userId: 1,
-        commentId: "65e09ebb-1622-4343-9bc8-223d9df7db01",
-    });
+    // await db.createReply({
+    //     body: "The comment was persuasive and intellectually stimulating",
+    //     userId: 1,
+    //     commentId: "65e09ebb-1622-4343-9bc8-223d9df7db01",
+    // });
+
+    await db.dislikeReply({ id: "bc6ec135-7c0c-4d18-b60a-89f19bb7c92a", userId: 2 });
 
     const query = await prisma.comment.findMany({
         include: {
-            replies: true,
+            replies: {
+                include: {
+                    likedBy: true,
+                },
+            },
         },
     });
 
