f7ec23750534ee47cfafb62dd5b0b936aec48a23
mastachii <mastachii273@gmail.com>
2025-03-02 17:06:02 +0800

    FEAT: Passport JWT configuration

--- backend/src/controller/userController.js
+++ backend/src/controller/userController.js
@@ -1,6 +1,6 @@
 const db = require("../model/userQueries");
 const bcrypt = require("bcryptjs");
-const { validateSignUp } = require("../helpers/userValidation");
+const { validateSignUp, validateLogIn } = require("../helpers/userValidation");
 const { validationResult } = require("express-validator");
 
 class User {
@@ -12,12 +12,25 @@ class User {
             if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
 
             bcrypt.hash(req.body.password, 10, async (err, hash) => {
+                if (err) return next(err);
+
                 req.body.password = hash;
 
                 await db.createUser(req.body);
 
                 res.sendStatus(201);
             });
+        },
+    ];
+
+    logIn = [
+        validateLogIn,
+        async (req, res) => {
+            const errors = validationResult(req);
+
+            if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
+
+            next();
         },
     ];
 }
---
+++ backend/src/passport/passport.js
@@ -0,0 +1,29 @@
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const bcrypt = require("bcryptjs");
+const { PrismaClient } = require("@prisma/client");
+
+const primsa = new PrismaClient();
+
+const options = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: process.env.SECRET,
+};
+
+const userStrategy = new JwtStrategy(options, async (payload, done) => {
+    try {
+        const user = await primsa.user.findUnique({
+            where: { username: payload.username },
+        });
+        if (!user) return done(null, false);
+
+        const passwordIsMatch = await bcrypt.compare(payload.password, user.password);
+        if (!passwordIsMatch) return done(null, false);
+
+        return done(null, user);
+    } catch (err) {
+        done(err);
+    }
+});
+
+module.exports = userStrategy;
