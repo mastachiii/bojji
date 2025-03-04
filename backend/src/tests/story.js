c7ed25b54f48f11e68c9257183dc314f2d728b8f
mastachii <mastachii273@gmail.com>
2025-03-04 12:16:02 +0800

    FEAT: Test suite for creating a new story

--- backend/src/tests/story.js
+++ backend/src/tests/story.js
@@ -1,3 +1,34 @@
+const getUserTokens = require("./helpers/getUserTokens");
 let request = require("supertest");
 
 request = request("http://localhost:8080");
+
+const storyTest = () => {
+    describe("Story test suite", () => {
+        let mastachiiToken;
+        let audreyToken;
+
+        beforeAll(async () => {
+            const tokens = await getUserTokens();
+
+            mastachiiToken = tokens[0];
+            audreyToken = tokens[1];
+        });
+
+        describe("Story CRUD Operations", () => {
+            it("Creates a story", async () => {
+                await request.post("/story/create").set("Authorization", `Bearer ${mastachiiToken}`).send({ image: "Some image" }).expect(201);
+
+                await request
+                    .get("/user/2")
+                    .set("Authorization", `Bearer ${mastachiiToken}`)
+                    .expect(200)
+                    .then(response => {
+                        const { user } = response.body;
+
+                        expect(user.stories).toHaveLength(1);
+                    });
+            });
+        });
+    });
+};
