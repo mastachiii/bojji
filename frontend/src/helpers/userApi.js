6c713b940098982ef42167d64ae2d040b9a15f3a
mastachii <mastachii273@gmail.com>
2025-03-07 00:04:25 +0800

    FEAT: Added function to user API helper for log ins

--- frontend/src/helpers/userApi.js
+++ frontend/src/helpers/userApi.js
@@ -4,20 +4,45 @@ class User {
     }
 
     async signUp({ username, email, password, passwordConfirm, fullName, errorHandler }) {
-        fetch(`${this.userUrl}/sign-up`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ username, email, password, fullName, passwordConfirm }),
-        })
-            .then(response => response.json())
-            .then(data => {
-                if (data.errors) return errorHandler(data.errors);
+        try {
+            fetch(`${this.userUrl}/sign-up`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ username, email, password, fullName, passwordConfirm }),
+            })
+                .then(response => response.json())
+                .then(data => {
+                    if (data.errors) return errorHandler(data.errors);
 
-                // TODO: Redirect user to index page when sign up is successful.
-                window.location.href = "/log-in";
-            });
+                    // TODO: Redirect user to index page when sign up is successful.
+                    window.location.href = "/log-in";
+                });
+        } catch (err) {
+            // TODO: Make a error page.
+        }
+    }
+
+    async logIn({ username, password, errorHandler }) {
+        try {
+            fetch(`${this.userUrl}/log-in`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ username, password }),
+            })
+                .then(response => response.json())
+                .then(data => {
+                    if (data.errors) return errorHandler(data.errors);
+
+                    localStorage.setItem("token", response.body.token);
+                    window.location.href = "/";
+                });
+        } catch (err) {
+            window.location.href = "/log-in";
+        }
     }
 }
 
