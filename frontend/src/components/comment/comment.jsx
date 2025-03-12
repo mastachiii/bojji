05126a469b5f75af761dbb14354ee5d9b451bb18
mastachii <mastachii273@gmail.com>
2025-03-13 06:59:08 +0800

    WIP: Like/dislike comments

--- frontend/src/components/comment/comment.jsx
+++ frontend/src/components/comment/comment.jsx
@@ -1,4 +1,11 @@
+import { useContext, useState } from "react";
+import userContext from "../context/userContext";
+
 export default function Comment({ comment }) {
+    const user = useContext(userContext);
+    const [likes, setLikes] = useState(comment.likedBy.length);
+    const [likedByUser, setLikedByUser] = useState(comment.likedBy.find(u => u.id === user.id));
+
     return (
         <span>
             <p>{comment.author.username}</p>
