6e441661fdc9bd86e6636016fdb415529b784b40
mastachii <mastachii273@gmail.com>
2025-03-15 17:00:18 +0800

    WIP: Styling for desktop layout login page

--- frontend/src/components/auth/form.jsx
+++ frontend/src/components/auth/form.jsx
@@ -6,7 +6,13 @@ export default function Form({ children, submitHandler, errors, label }) {
             <div>
                 <h3>{label}</h3>
                 <div className="flex flex-col items-center gap-3">{children}</div>
-                <div>{errors && <p className="w-[60%] text-center ml-auto mr-auto mt-4 text-sm text-red-400">Sorry, your password incorrect. Please double-check your password.</p>}</div>
+                <div>
+                    {errors && (
+                        <p className="w-[60%] text-center ml-auto mr-auto mt-4 text-sm text-red-400">
+                            Sorry, your password incorrect. Please double-check your password.
+                        </p>
+                    )}
+                </div>
             </div>
         </form>
     );
