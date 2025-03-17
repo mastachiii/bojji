a96a6f6aa87e98273b092249df3084c48b06a1bc
mastachii <mastachii273@gmail.com>
2025-03-18 05:13:37 +0800

    FEAT: Desktop layout for profile page

--- frontend/src/components/user/emptyPosts.jsx
+++ frontend/src/components/user/emptyPosts.jsx
@@ -2,8 +2,8 @@ import camera from "../../assets/camera.svg";
 
 export default function EmptyPosts() {
     return (
-        <div className="w-screen pt-20 flex flex-col justify-center items-center">
-            <img src={camera} className="size-20"/>
+        <div className="w-screen pt-20 flex flex-col justify-center items-center md:w-[50vw]">
+            <img src={camera} className="size-20 mb-2"/>
             <p className="font-semibold">No Posts Yet</p>
         </div>
     );
--- frontend/src/components/user/profile.jsx
+++ frontend/src/components/user/profile.jsx
@@ -5,6 +5,7 @@ import userContext from "../context/userContext";
 import Back from "../../assets/back.svg";
 import ProfileButton from "./profileButton";
 import EmptyPosts from "./emptyPosts";
+import { Link } from "react-router";
 
 export default function Profile() {
     const [user, setUser] = useState(null);
@@ -20,7 +21,7 @@ export default function Profile() {
         })();
     }, [params.username]);
 
-    if (user) {
+    if (user && userData) {
         console.log({ user });
         const isUser = user.id === userData.id;
         const followingUser = userData.following.find(u => u.id === user.id);
@@ -30,47 +31,61 @@ export default function Profile() {
             userApi.interactWithUser({ username: user.username, type: followingUser ? "unfollow" : "follow", statusHandler: setStatus });
         }
 
+        const interactBtns = (
+            <>
+                <ProfileButton
+                    label={isUser ? "Edit Profile" : followingUser ? "Unfollow" : "Follow"}
+                    handler={handleInteraction}
+                    btnActive={status !== "INTERACTING"}
+                />
+                {!isUser && <ProfileButton label={"Message"} btnActive={true} />}
+            </>
+        );
+
         return (
-            <div className="font-sans">
-                <div className="w-full flex pl-3 pb-2 pt-2 border-b-1 border-neutral-200">
-                    <button className="w-[10%]">
+            <div className="font-sans md:w-[50%] md:ml-auto md:mr-auto md:mt-2">
+                <div className="w-full flex pl-3 pb-2 pt-2 border-b-1 border-neutral-200 md:hidden">
+                    <Link to="/" className="w-[10%]">
                         <img src={Back} className="size-4" />
-                    </button>
+                    </Link>
                     <p className="w-[80%] text-sm font-semibold text-center">{user.username}</p>
                 </div>
                 <div className="flex items-center p-3">
-                    <img src={user.profilePicture} className="size-17 mt-3 rounded-full" />
-                    <span className="flex gap-7 justify-center ml-10">
-                        <span className="flex flex-col items-center text-xs">
-                            <p className="font-semibold">{user.posts.length}</p>
-                            <p className="text-neutral-500">posts</p>
+                    <img src={user.profilePicture} className="size-17 mt-3 rounded-full md:size-25" />
+                    <div>
+                        <span className={`hidden ml-10 md:flex`}>
+                            <p className="mr-4">{user.username}</p>
+                            <span className="flex gap-2 mb-4">{interactBtns}</span>
                         </span>
-                        <span className="flex flex-col items-center text-xs">
-                            <p className="font-semibold">{user.followers.length}</p>
-                            <p className="text-neutral-500">followers</p>
+                        <span className="flex gap-7 justify-center ml-10 ">
+                            <span className="flex flex-col items-center text-xs md:flex-row md:gap-1">
+                                <p className="font-semibold">{user.posts.length}</p>
+                                <p className="text-neutral-500">posts</p>
+                            </span>
+                            <span className="flex flex-col items-center text-xs md:flex-row md:gap-1">
+                                <p className="font-semibold">{user.followers.length}</p>
+                                <p className="text-neutral-500">followers</p>
+                            </span>
+                            <span className="flex flex-col items-center text-xs md:flex-row md:gap-1">
+                                <p className="font-semibold">{user.following.length}</p>
+                                <p className="text-neutral-500">following</p>
+                            </span>
                         </span>
-                        <span className="flex flex-col items-center text-xs">
-                            <p className="font-semibold">{user.following.length}</p>
-                            <p className="text-neutral-500">following</p>
-                        </span>
-                    </span>
+                    </div>
                 </div>
-                <div className="pl-4 pr-4 pb-7 border-b-1 border-neutral-200">
+                <div className="pl-4 pr-4 pb-7 border-b-1 border-neutral-200 md:pl-41">
                     <h3 className="text-xs font-semibold">{user.fullName}</h3>
                     <p className="text-xs">{user.bio}</p>
-                    <span className="flex gap-2 mt-5">
-                        <ProfileButton
-                            label={isUser ? "Edit Profile" : followingUser ? "Unfollow" : "Follow"}
-                            handler={handleInteraction}
-                            btnActive={status !== "INTERACTING"}
-                        />
-                        {!isUser && <ProfileButton label={"Message"} btnActive={true}/>}
-                    </span>
+                    <span className="flex gap-2 mt-5 md:hidden">{interactBtns}</span>
                 </div>
-                <div className="grid grid-cols-3 gap-1 mt-1">
-                    {user.posts.length >= 1 ? user.posts.map(p => {
-                        return <img src={p.images[0]} className="w-full min-h-40 max-h-40" />;
-                    }) : <EmptyPosts />}
+                <div className="grid grid-cols-3 gap-1 mt-1 justify-center">
+                    {user.posts.length >= 1 ? (
+                        user.posts.map(p => {
+                            return <img src={p.images[0]} className="w-full min-h-40 md:min-h-70" />;
+                        })
+                    ) : (
+                        <EmptyPosts />
+                    )}
                 </div>
             </div>
         );
