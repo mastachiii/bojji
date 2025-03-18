65326c4fb5adf5dde8c054f182bf7dd91c1b08a7
mastachii <mastachii273@gmail.com>
2025-03-19 06:50:19 +0800

    WIP: Styling edit profile component

--- frontend/src/components/user/editProfile.jsx
+++ frontend/src/components/user/editProfile.jsx
@@ -1,17 +1,17 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import FormField from "../auth/formField";
 import userApi from "../../helpers/userApi";
 import EditProfileField from "./editProfileField";
+import userContext from "../context/userContext";
 
 export default function EditProfile() {
-    const [username, setUsername] = useState("");
-    const [fullName, setFullName] = useState("");
-    const [bio, setBio] = useState("");
+    const userData = useContext(userContext) || {};
+    const [username, setUsername] = useState(userData.username);
+    const [fullName, setFullName] = useState(userData.fullName);
+    const [bio, setBio] = useState(userData.bio);
     const [profilePicture, setProfilePicture] = useState(null);
 
-    function handleSubmit(e) {
-        e.preventDefault();
-
+    function handleUpdate() {
         userApi.updateProfile({ username, fullName, bio, profilePicture });
     }
 
@@ -19,12 +19,17 @@ export default function EditProfile() {
         <div>
             <div>
                 <h1>Profile picture</h1>
-                <img src={profilePicture && URL.createObjectURL(profilePicture)} />
+                <img src={profilePicture ? URL.createObjectURL(profilePicture) : userData.profilePicture} className="size-10" />
                 <input type="file" onChange={e => setProfilePicture(e.target.files[0])} />
+                <button onClick={() => setProfilePicture(null)}>clear</button>
             </div>
             <EditProfileField value={fullName} handler={setFullName} label={"Full Name:"} />
-            <EditProfileField value={bio} handler={setBio} label={"Bio:"} />
             <EditProfileField value={username} handler={setUsername} label={"Username:"} />
+            <div>
+                <h1>Bio</h1>
+                <textarea value={bio} onChange={e => setBio(e.target.value)}></textarea>
+            </div>
+            <button onClick={handleUpdate}>Update</button>
         </div>
     );
 }
