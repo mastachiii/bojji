75428654977039be22267dd6be3350a6b351eeb0
mastachii <mastachii273@gmail.com>
2025-03-27 17:04:26 +0800

    FIX: Vercel config file was in the wrong folder

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,8 +4,6 @@ import { BrowserRouter, Route, RouterProvider, Routes } from "react-router";
 import Wrapper from "./components/wrapper";
 import routes from "./routes";
 import "./index.css";
-import LogIn from "./components/auth/logIn";
-import SignUp from "./components/auth/signUp";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
